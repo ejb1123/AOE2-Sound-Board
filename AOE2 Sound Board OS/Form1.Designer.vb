9767fc836367a04ff5e88127f84ca17ae340a22e
E.J. Bevenour <ej@ejb1123.tk>
2013-10-13 03:27:38 -0400

    initial commit

---
+++ AOE2 Sound Board OS/Form1.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.Button24 = New System.Windows.Forms.Button()
+        Me.Button25 = New System.Windows.Forms.Button()
+        Me.Button26 = New System.Windows.Forms.Button()
+        Me.Button27 = New System.Windows.Forms.Button()
+        Me.Button28 = New System.Windows.Forms.Button()
+        Me.Button29 = New System.Windows.Forms.Button()
+        Me.Button30 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(76, 53)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Yes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(94, 24)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(76, 53)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "No"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(176, 24)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(76, 53)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Food Please"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(258, 24)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(76, 53)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Wood Please"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(340, 24)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(76, 53)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Gold Please"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(422, 24)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(76, 53)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Stone Please"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(504, 24)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(76, 53)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Ahh"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(588, 24)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(76, 53)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "All Hail"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(670, 24)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(76, 53)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Oooh"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(752, 24)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(76, 53)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Back to AOE"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(12, 83)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(76, 53)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "Herb Laugh"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(94, 83)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(76, 53)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "Being Rushed"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(176, 83)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(76, 53)
+        Me.Button13.TabIndex = 12
+        Me.Button13.Text = "Blame it on ISP"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(258, 83)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(76, 53)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "Start the Game"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(340, 83)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(76, 53)
+        Me.Button15.TabIndex = 14
+        Me.Button15.Text = "Don't Point that Thing"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(422, 83)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(76, 53)
+        Me.Button16.TabIndex = 15
+        Me.Button16.Text = "Enemy Sighted"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(504, 83)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(76, 53)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = "It is Good"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(588, 83)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(76, 53)
+        Me.Button18.TabIndex = 17
+        Me.Button18.Text = "I Need a Monk"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(670, 83)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(76, 53)
+        Me.Button19.TabIndex = 18
+        Me.Button19.Text = "Long Time no Siege"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(752, 83)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(76, 53)
+        Me.Button20.TabIndex = 19
+        Me.Button20.Text = "My Granny"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(12, 142)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(76, 53)
+        Me.Button21.TabIndex = 20
+        Me.Button21.Text = "Nice Town I'll Take it"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Location = New System.Drawing.Point(94, 142)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(76, 53)
+        Me.Button22.TabIndex = 21
+        Me.Button22.Text = "Quit Touchin"
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Location = New System.Drawing.Point(176, 142)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(76, 53)
+        Me.Button23.TabIndex = 22
+        Me.Button23.Text = "Raiding Party"
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'Button24
+        '
+        Me.Button24.Location = New System.Drawing.Point(258, 142)
+        Me.Button24.Name = "Button24"
+        Me.Button24.Size = New System.Drawing.Size(76, 53)
+        Me.Button24.TabIndex = 23
+        Me.Button24.Text = "Dadgum"
+        Me.Button24.UseVisualStyleBackColor = True
+        '
+        'Button25
+        '
+        Me.Button25.Location = New System.Drawing.Point(340, 142)
+        Me.Button25.Name = "Button25"
+        Me.Button25.Size = New System.Drawing.Size(76, 53)
+        Me.Button25.TabIndex = 24
+        Me.Button25.Text = "Smite Me"
+        Me.Button25.UseVisualStyleBackColor = True
+        '
+        'Button26
+        '
+        Me.Button26.Location = New System.Drawing.Point(422, 142)
+        Me.Button26.Name = "Button26"
+        Me.Button26.Size = New System.Drawing.Size(76, 53)
+        Me.Button26.TabIndex = 25
+        Me.Button26.Text = "The Wonder"
+        Me.Button26.UseVisualStyleBackColor = True
+        '
+        'Button27
+        '
+        Me.Button27.Location = New System.Drawing.Point(504, 142)
+        Me.Button27.Name = "Button27"
+        Me.Button27.Size = New System.Drawing.Size(76, 53)
+        Me.Button27.TabIndex = 26
+        Me.Button27.Text = "You Play Two Hours"
+        Me.Button27.UseVisualStyleBackColor = True
+        '
+        'Button28
+        '
+        Me.Button28.Location = New System.Drawing.Point(588, 142)
+        Me.Button28.Name = "Button28"
+        Me.Button28.Size = New System.Drawing.Size(76, 53)
+        Me.Button28.TabIndex = 27
+        Me.Button28.Text = "You Should see the Other Guy"
+        Me.Button28.UseVisualStyleBackColor = True
+        '
+        'Button29
+        '
+        Me.Button29.Location = New System.Drawing.Point(670, 142)
+        Me.Button29.Name = "Button29"
+        Me.Button29.Size = New System.Drawing.Size(76, 53)
+        Me.Button29.TabIndex = 28
+        Me.Button29.Text = "Roggan"
+        Me.Button29.UseVisualStyleBackColor = True
+        '
+        'Button30
+        '
+        Me.Button30.Location = New System.Drawing.Point(752, 142)
+        Me.Button30.Name = "Button30"
+        Me.Button30.Size = New System.Drawing.Size(76, 53)
+        Me.Button30.TabIndex = 29
+        Me.Button30.Text = "Wololo"
+        Me.Button30.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 219)
+        Me.Controls.Add(Me.Button30)
+        Me.Controls.Add(Me.Button29)
+        Me.Controls.Add(Me.Button28)
+        Me.Controls.Add(Me.Button27)
+        Me.Controls.Add(Me.Button26)
+        Me.Controls.Add(Me.Button25)
+        Me.Controls.Add(Me.Button24)
+        Me.Controls.Add(Me.Button23)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.Button21)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AOE Sound Board"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button17 As System.Windows.Forms.Button
+    Friend WithEvents Button18 As System.Windows.Forms.Button
+    Friend WithEvents Button19 As System.Windows.Forms.Button
+    Friend WithEvents Button20 As System.Windows.Forms.Button
+    Friend WithEvents Button21 As System.Windows.Forms.Button
+    Friend WithEvents Button22 As System.Windows.Forms.Button
+    Friend WithEvents Button23 As System.Windows.Forms.Button
+    Friend WithEvents Button24 As System.Windows.Forms.Button
+    Friend WithEvents Button25 As System.Windows.Forms.Button
+    Friend WithEvents Button26 As System.Windows.Forms.Button
+    Friend WithEvents Button27 As System.Windows.Forms.Button
+    Friend WithEvents Button28 As System.Windows.Forms.Button
+    Friend WithEvents Button29 As System.Windows.Forms.Button
+    Friend WithEvents Button30 As System.Windows.Forms.Button
+
+End Class
---
+++ AOE2 Sound Board OS/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports System.Media.SoundPlayer
+Imports Microsoft.Win32
+
+Public Class Form1
+    Dim rootDir As String
+    Dim a As new MediaPlayer.MediaPlayer
+    Public Sub getInstallPath() Handles Me.Load
+        Dim key As RegistryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Microsoft Games\Age of Empires\2.0")
+        rootDir = key.GetValue("InstallationDirectory")
+    End Sub
+    Public Sub playTaunt(ByRef source As String)
+        a.FileName = rootDir + "\" + source
+        a.Play()
+        getInstallPath()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        playTaunt(My.Resources._01_Yes)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        playTaunt(My.Resources._02_No)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        playTaunt(My.Resources._03_Food__please)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        playTaunt(My.Resources._04_Wood__please)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        playTaunt(My.Resources._05_Gold__PLease)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        playTaunt(My.Resources._06_Stone__please)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        playTaunt(My.Resources._07_Ahh)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        playTaunt(My.Resources._08_All_hail)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        playTaunt(My.Resources._09_Oooh)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        playTaunt(My.Resources._10_Back_to_Age_1)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        playTaunt(My.Resources._11_Herb_laugh)
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        playTaunt(My.Resources._12_Being_rushed)
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        playTaunt(My.Resources._13_Blame_your_isp)
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        playTaunt(My.Resources._14_Start_the_game)
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        playTaunt(My.Resources._15_Don_t_Point_That_Thing)
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        playTaunt(My.Resources._16_Enemy_Sighted)
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        playTaunt(My.Resources._17_It_Is_Good)
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        playTaunt(My.Resources._18_I_Need_a_Monk)
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        playTaunt(My.Resources._19_Long_Time_No_Siege)
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        playTaunt(My.Resources._20_My_granny)
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        playTaunt(My.Resources._21_Nice_Town_I_ll_Take_It)
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        playTaunt(My.Resources._22_Quit_Touchin)
+    End Sub
+
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        playTaunt(My.Resources._23_Raiding_Party)
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click
+        playTaunt(My.Resources._24_Dadgum)
+    End Sub
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
+        playTaunt(My.Resources._25_Smite_Me)
+    End Sub
+
+    Private Sub Button26_Click(sender As Object, e As EventArgs) Handles Button26.Click
+        playTaunt(My.Resources._26_The_wonder)
+    End Sub
+
+    Private Sub Button27_Click(sender As Object, e As EventArgs) Handles Button27.Click
+        playTaunt(My.Resources._27_You_play_2_hours)
+    End Sub
+
+    Private Sub Button28_Click(sender As Object, e As EventArgs) Handles Button28.Click
+        playTaunt(My.Resources._28_You_Should_See_the_Other_Guy)
+    End Sub
+
+    Private Sub Button29_Click(sender As Object, e As EventArgs) Handles Button29.Click
+        playTaunt(My.Resources._29_Roggan)
+    End Sub
+
+    Private Sub Button30_Click(sender As Object, e As EventArgs) Handles Button30.Click
+        playTaunt(My.Resources._30_Wololo)
+    End Sub
+End Class
