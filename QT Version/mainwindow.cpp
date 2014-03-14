c394f6a9f4dc101bcb60ccff14e3fca062aeb63c
E.J. Bevenour <ej@ejb1123.tk>
2014-03-13 21:59:56 -0400

    Beta 1

--- QT Version/mainwindow.cpp
+++ QT Version/mainwindow.cpp
@@ -48,3 +48,123 @@ void MainWindow::on_pushButton_6_clicked()
 {
     Player("06");
 }
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    Player("07");
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    Player("08");
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    Player("09");
+}
+
+void MainWindow::on_pushButton_10_clicked()
+{
+    Player("10");
+}
+
+void MainWindow::on_pushButton_11_clicked()
+{
+    Player("11");
+}
+
+void MainWindow::on_pushButton_12_clicked()
+{
+    Player("12");
+}
+
+void MainWindow::on_pushButton_13_clicked()
+{
+    Player("13");
+}
+
+void MainWindow::on_pushButton_14_clicked()
+{
+    Player("14");
+}
+
+void MainWindow::on_pushButton_15_clicked()
+{
+    Player("15");
+}
+
+void MainWindow::on_pushButton_16_clicked()
+{
+    Player("16");
+}
+
+void MainWindow::on_pushButton_17_clicked()
+{
+    Player("17");
+}
+
+void MainWindow::on_pushButton_18_clicked()
+{
+    Player("18");
+}
+
+void MainWindow::on_pushButton_19_clicked()
+{
+    Player("19");
+}
+
+void MainWindow::on_pushButton_20_clicked()
+{
+    Player("20");
+}
+
+void MainWindow::on_pushButton_21_clicked()
+{
+    Player("21");
+}
+
+void MainWindow::on_pushButton_22_clicked()
+{
+    Player("22");
+}
+
+void MainWindow::on_pushButton_23_clicked()
+{
+    Player("23");
+}
+
+void MainWindow::on_pushButton_24_clicked()
+{
+    Player("24");
+}
+
+void MainWindow::on_pushButton_25_clicked()
+{
+    Player("25");
+}
+
+void MainWindow::on_pushButton_26_clicked()
+{
+    Player("26");
+}
+
+void MainWindow::on_pushButton_27_clicked()
+{
+    Player("27");
+}
+
+void MainWindow::on_pushButton_28_clicked()
+{
+    Player("28");
+}
+
+void MainWindow::on_pushButton_29_clicked()
+{
+    Player("29");
+}
+
+void MainWindow::on_pushButton_30_clicked()
+{
+ Player("30");
+}
